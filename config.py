3f5d3010634916d3939ecfdcc7032617493bd815
varun29-git <vjdaiya29@gmail.com>
2026-01-24 19:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ LR = 3e-4
 WEIGHT_DECAY = 0.01
 
 # Model hyperparameters
-D_MODEL = 512
+D_MODEL = 256
 H = 8  # Number of attention heads
 N = 6  # Number of decoder layers
-D_FF = 2048  # Feed-forward dimension
+D_FF = 1024  # Feed-forward dimension
 DROPOUT = 0.1
 
 # Training settings
